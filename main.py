f56bfee9122fbd23995b1e3b9ed922e6838cc39a
bhavishya75459 <js2432601@gmail.com>
2026-02-26 09:14:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,38 +20,43 @@ MDScreen:
 
 # Android AdMob Setup
 if platform == "android":
-    from jnius import autoclass
-    from android.runnable import run_on_ui_thread
-
-    AdView = autoclass('com.google.android.gms.ads.AdView')
-    AdSize = autoclass('com.google.android.gms.ads.AdSize')
-    AdRequestBuilder = autoclass('com.google.android.gms.ads.AdRequest$Builder')
-    MobileAds = autoclass('com.google.android.gms.ads.MobileAds')
-    PythonActivity = autoclass('org.kivy.android.PythonActivity')
-    FrameLayoutParams = autoclass('android.widget.FrameLayout$LayoutParams')
+    try:
+        from jnius import autoclass
+        from android.runnable import run_on_ui_thread
+    
+        AdView = autoclass('com.google.android.gms.ads.AdView')
+        AdSize = autoclass('com.google.android.gms.ads.AdSize')
+        AdRequestBuilder = autoclass('com.google.android.gms.ads.AdRequest$Builder')
+        MobileAds = autoclass('com.google.android.gms.ads.MobileAds')
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        FrameLayoutParams = autoclass('android.widget.FrameLayout$LayoutParams')
+    except Exception as e:
+        pass
 
     @run_on_ui_thread
     def show_banner():
-        activity = PythonActivity.mActivity
-        MobileAds.initialize(activity)
+        try:
+            activity = PythonActivity.mActivity
+            MobileAds.initialize(activity)
+    
+            adview = AdView(activity)
+            adview.setAdSize(AdSize.BANNER)
+    
+            # ✅ Test Banner Ad Unit ID
+            adview.setAdUnitId("ca-app-pub-7264801834502563/6725695296")
+    
+            adRequest = AdRequestBuilder().build()
+            adview.loadAd(adRequest)
+    
+            params = FrameLayoutParams(
+                FrameLayoutParams.MATCH_PARENT,
+                FrameLayoutParams.WRAP_CONTENT
+            )
+    
+            activity.addContentView(adview, params)
 
-        adview = AdView(activity)
-        adview.setAdSize(AdSize.BANNER)
-
-        # ✅ Test Banner Ad Unit ID
-        adview.setAdUnitId("ca-app-pub-7264801834502563/6725695296")
-
-        adRequest = AdRequestBuilder().build()
-        adview.loadAd(adRequest)
-
-        params = FrameLayoutParams(
-            FrameLayoutParams.MATCH_PARENT,
-            FrameLayoutParams.WRAP_CONTENT
-        )
-
-        activity.addContentView(adview, params)
-
-
+        except Exception as e:
+            pass
 class AdApp(MDApp):
     def build(self):
         return Builder.load_string(KV)
